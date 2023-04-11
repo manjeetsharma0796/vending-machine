13d385766edbdc3d4586d98b297d13f8cc3f0044
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-04-11 19:24:02 +0530

    Removing sample.txt

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,43 +1,53 @@
-const vendCoins = function(amount) {
+const vendCoins = function(amount, denominationSet) {
   let oneRupeeCoin;
   let twoRupeeCoin;
   let fiveRupeeCoin;
   let tenRupeeCoin;
-  let remaining;
+  let remaining = amount;
   let total = 0;
+  for ( ; 0 <= denominationSet.length; ) {
+    switch (denominationSet.pop()) {
+      case 10:  if (remaining !== 0) {
+        tenRupeeCoin = Math.floor(remaining / 10);
+        remaining = remaining - tenRupeeCoin * 10;
+        total += tenRupeeCoin;
+      } else {
+        return total;
+      };
 
-  if (amount !== 0) {
-    tenRupeeCoin = Math.floor(amount / 10);
-    remaining = amount - tenRupeeCoin * 10;
-    total += tenRupeeCoin;
-  } else {
-    return total;
+        break;
+
+      case 5:  if (remaining !== 0) {
+        fiveRupeeCoin = Math.floor(remaining / 5);
+        remaining = remaining - fiveRupeeCoin * 5;
+        total += fiveRupeeCoin;
+      } else {
+        return total;
+      };
+
+        break;
+
+      case 2: if (remaining !== 0) {
+        twoRupeeCoin = Math.floor(remaining / 2);
+        remaining = remaining - twoRupeeCoin * 2;
+        total += twoRupeeCoin;
+      }else {
+        return total;
+      };
+
+        break;
+
+      case 1: if (remaining !== 0) {
+        oneRupeeCoin = Math.floor(remaining / 1);
+        remaining = remaining - oneRupeeCoin * 1;
+        total += oneRupeeCoin;
+      } else {
+        return total;
+      };
+    }
   }
-  if (remaining !== 0) {
-    fiveRupeeCoin = Math.floor(remaining / 5);
-    remaining = remaining - fiveRupeeCoin * 5;
-    total += fiveRupeeCoin;
-  } else {
-    return total;
-  };
-
-  if (remaining !== 0) {
-    twoRupeeCoin = Math.floor(remaining / 2);
-    remaining = remaining - twoRupeeCoin * 2;
-    total += twoRupeeCoin;
-  }else {
-    return total;
-  };
-
-  if (remaining !== 0) {
-    oneRupeeCoin = Math.floor(remaining / 1);
-    remaining = remaining - oneRupeeCoin * 1;
-    total += oneRupeeCoin;
-  } else {
-    return total;
-  };
 
   return total;
-}
+};
 
 exports.vendCoins = vendCoins;
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -3,13 +3,13 @@ const test = require("../lib/testing.js");
 
 const vendingTest = function() {
   test.displayHeadline("vendingMachineTest");
-  test.assertEquals(vending.vendCoins(1), 1, "It should give 1 coin for INR 1");
+  test.assertEquals(vending.vendCoins(1), [1], "It should give 1 coin for INR 1");
   test.assertEquals(vending.vendCoins(2), 1, "It should give 1 coin for INR 2");
   test.assertEquals(vending.vendCoins(3), 2, "It should give 2 coin for INR 3");
   test.assertEquals(vending.vendCoins(4), 2, "It should give 2 coin for INR 4");
   test.assertEquals(vending.vendCoins(5), 1, "It should give 1 coin for INR 5");
   test.assertEquals(vending.vendCoins(10), 1, "It should give 1 coin for INR 10");
-  test.assertEquals(vending.vendCoins(13), 3, "It should give 1 coin for INR 13");
+  test.assertEquals(vending.vendCoins(13), 3, "It should give 3 coin for INR 13");
   test.assertEquals(vending.vendCoins(15), 2, "It should give 1 coin for INR 15");
 }
 vendingTest();

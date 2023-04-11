43905e4c74c46bb8029f08e2428efa07f0ffd4be
Manjeet Sharma <manjeet@Manjeets-MacBook-Pro.local>
2023-04-11 17:20:44 +0530

    Added INR 2 and INR 5 vending

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,6 +1,35 @@
-const vendingMachine = function(amount) {
-  let coins = amount;
-  return coins;
+const vendCoins = function(amount) {
+  let oneRupeeCoin;
+  let twoRupeeCoin;
+  let fiveRupeeCoin;
+  let remaining;
+  let total = 0;
+
+  if (amount !== 0) {
+    fiveRupeeCoin = Math.floor(amount / 5);
+    remaining = amount - fiveRupeeCoin * 5;
+    total += fiveRupeeCoin;
+  } else {
+    return total;
+  };
+
+  if (remaining !== 0) {
+    twoRupeeCoin = Math.floor(remaining / 2);
+    remaining = remaining - twoRupeeCoin * 2;
+    total += twoRupeeCoin;
+  }else {
+    return total;
+  };
+
+  if (remaining !== 0) {
+    oneRupeeCoin = Math.floor(remaining / 1);
+    remaining = remaining - oneRupeeCoin * 1;
+    total += oneRupeeCoin;
+  } else {
+    return total;
+  };
+
+  return total;
 }
 
-exports.vendingMachine = vendingMachine;
+exports.vendCoins = vendCoins;
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -3,8 +3,10 @@ const test = require("../lib/testing.js");
 
 const vendingTest = function() {
   test.displayHeadline("vendingMachineTest");
-  test.assertEquals(vending.vendingMachine(1), 1, "It should return 1 for INR 1");
-  test.assertEquals(vending.vendingMachine(2), 2, "It should return 2 for INR 2");
-  test.assertEquals(vending.vendingMachine(3), 3, "It should return 3 for INR 3");
+  test.assertEquals(vending.vendCoins(1), 1, "It should give 1 coin for INR 1");
+  test.assertEquals(vending.vendCoins(2), 1, "It should give 1 coin for INR 2");
+  test.assertEquals(vending.vendCoins(3), 2, "It should give 2 coin for INR 3");
+  test.assertEquals(vending.vendCoins(4), 2, "It should give 2 coin for INR 4");
+  test.assertEquals(vending.vendCoins(5), 1, "It should give 1 coin for INR 3");
 }
 vendingTest();

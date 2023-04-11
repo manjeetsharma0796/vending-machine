43aa1900476925d8d8d2a6376c02fb9997007f82
Manjeet Sharma <manjeet@Manjeets-MacBook-Pro.local>
2023-04-11 15:13:13 +0530

    Test message is working

--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -3,7 +3,7 @@ const test = require("../lib/testing.js");
 
 const vendingTest = function() {
   test.displayHeadline("vendingMachineTest");
-  test.assertEquals(vending.vendingMachine(), true), "This is false";
+  test.assertEquals(vending.vendingMachine(), true, "This is false");
 }
 
 vendingTest();

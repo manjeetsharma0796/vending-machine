72884d5ec2515fbd88bde9b0911160de8d01a31f
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-04-12 10:42:18 +0530

    Refactored the vendingCoin function

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -6,49 +6,32 @@ const vendCoins = function(amount, denominationSet) {
   let remainingAmount = amount;
   let totalCoin = 0;
   for (let index = denominationSet.length - 1 ; index >= 0; index--) {
-    switch (denominationSet[index]) {
+    if (remainingAmount !== 0) {
+      switch (denominationSet[index]) {
 
-      case 10:  if (remainingAmount !== 0) {
-        tenRupeeCoin = Math.floor(remainingAmount / 10);
-        remainingAmount = remainingAmount - tenRupeeCoin * 10;
-        totalCoin += tenRupeeCoin;
-      } else {
-        return totalCoin;
-      };
+        case 10: tenRupeeCoin = Math.floor(remainingAmount / 10);
+          remainingAmount = remainingAmount - tenRupeeCoin * 10;
+          totalCoin += tenRupeeCoin;
+          break;
 
-        break;
+        case 5: fiveRupeeCoin = Math.floor(remainingAmount / 5);
+          remainingAmount = remainingAmount - fiveRupeeCoin * 5;
+          totalCoin += fiveRupeeCoin;
+          break;
 
-      case 5:  if (remainingAmount !== 0) {
-        fiveRupeeCoin = Math.floor(remainingAmount / 5);
-        remainingAmount = remainingAmount - fiveRupeeCoin * 5;
-        totalCoin += fiveRupeeCoin;
-      } else {
-        return totalCoin;
-      };
+        case 2: twoRupeeCoin = Math.floor(remainingAmount / 2);
+          remainingAmount = remainingAmount - twoRupeeCoin * 2;
+          totalCoin += twoRupeeCoin;
+          break;
 
-        break;
+        case 1: oneRupeeCoin = Math.floor(remainingAmount / 1);
+          remainingAmount = remainingAmount - oneRupeeCoin * 1;
+          totalCoin += oneRupeeCoin;
+          break;
+      }
+    };
 
-      case 2: if (remainingAmount !== 0) {
-        twoRupeeCoin = Math.floor(remainingAmount / 2);
-        remainingAmount = remainingAmount - twoRupeeCoin * 2;
-        totalCoin += twoRupeeCoin;
-      }else {
-        return totalCoin;
-      };
-
-        break;
-
-      case 1: if (remainingAmount !== 0) {
-        oneRupeeCoin = Math.floor(remainingAmount / 1);
-        remainingAmount = remainingAmount - oneRupeeCoin * 1;
-        totalCoin += oneRupeeCoin;
-      } else {
-        return totalCoin;
-      };
-
-        break;
-    }
-  }
+  };
 
   return totalCoin;
 };

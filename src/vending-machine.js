84d38443f053d50d87d2b26b304a19d40e587645
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-04-12 08:35:06 +0530

    Added Denomination set to the function

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -3,51 +3,54 @@ const vendCoins = function(amount, denominationSet) {
   let twoRupeeCoin;
   let fiveRupeeCoin;
   let tenRupeeCoin;
-  let remaining = amount;
-  let total = 0;
-  for ( ; 0 <= denominationSet.length; ) {
-    switch (denominationSet.pop()) {
-      case 10:  if (remaining !== 0) {
-        tenRupeeCoin = Math.floor(remaining / 10);
-        remaining = remaining - tenRupeeCoin * 10;
-        total += tenRupeeCoin;
+  let remainingAmount = amount;
+  let totalCoin = 0;
+  for (let index = denominationSet.length - 1 ; index >= 0; index--) {
+    switch (denominationSet[index]) {
+
+      case 10:  if (remainingAmount !== 0) {
+        tenRupeeCoin = Math.floor(remainingAmount / 10);
+        remainingAmount = remainingAmount - tenRupeeCoin * 10;
+        totalCoin += tenRupeeCoin;
       } else {
-        return total;
+        return totalCoin;
       };
 
         break;
 
-      case 5:  if (remaining !== 0) {
-        fiveRupeeCoin = Math.floor(remaining / 5);
-        remaining = remaining - fiveRupeeCoin * 5;
-        total += fiveRupeeCoin;
+      case 5:  if (remainingAmount !== 0) {
+        fiveRupeeCoin = Math.floor(remainingAmount / 5);
+        remainingAmount = remainingAmount - fiveRupeeCoin * 5;
+        totalCoin += fiveRupeeCoin;
       } else {
-        return total;
+        return totalCoin;
       };
 
         break;
 
-      case 2: if (remaining !== 0) {
-        twoRupeeCoin = Math.floor(remaining / 2);
-        remaining = remaining - twoRupeeCoin * 2;
-        total += twoRupeeCoin;
+      case 2: if (remainingAmount !== 0) {
+        twoRupeeCoin = Math.floor(remainingAmount / 2);
+        remainingAmount = remainingAmount - twoRupeeCoin * 2;
+        totalCoin += twoRupeeCoin;
       }else {
-        return total;
+        return totalCoin;
       };
 
         break;
 
-      case 1: if (remaining !== 0) {
-        oneRupeeCoin = Math.floor(remaining / 1);
-        remaining = remaining - oneRupeeCoin * 1;
-        total += oneRupeeCoin;
+      case 1: if (remainingAmount !== 0) {
+        oneRupeeCoin = Math.floor(remainingAmount / 1);
+        remainingAmount = remainingAmount - oneRupeeCoin * 1;
+        totalCoin += oneRupeeCoin;
       } else {
-        return total;
+        return totalCoin;
       };
+
+        break;
     }
   }
 
-  return total;
+  return totalCoin;
 };
 
 exports.vendCoins = vendCoins;
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -3,13 +3,15 @@ const test = require("../lib/testing.js");
 
 const vendingTest = function() {
   test.displayHeadline("vendingMachineTest");
-  test.assertEquals(vending.vendCoins(1), [1], "It should give 1 coin for INR 1");
-  test.assertEquals(vending.vendCoins(2), 1, "It should give 1 coin for INR 2");
-  test.assertEquals(vending.vendCoins(3), 2, "It should give 2 coin for INR 3");
-  test.assertEquals(vending.vendCoins(4), 2, "It should give 2 coin for INR 4");
-  test.assertEquals(vending.vendCoins(5), 1, "It should give 1 coin for INR 5");
-  test.assertEquals(vending.vendCoins(10), 1, "It should give 1 coin for INR 10");
-  test.assertEquals(vending.vendCoins(13), 3, "It should give 3 coin for INR 13");
-  test.assertEquals(vending.vendCoins(15), 2, "It should give 1 coin for INR 15");
+  test.assertEquals(vending.vendCoins(1, [1]), 1 , "It should give 1 coin for INR 1 when the set is Rs1");
+  test.assertEquals(vending.vendCoins(2, [1]), 2, "It should give 2 coin for INR 2 when the set is Rs1");
+  test.assertEquals(vending.vendCoins(3, [1]), 3, "It should give 3 coin for INR 3 when the set is Rs1");
+  test.assertEquals(vending.vendCoins(1, [2]), 0, "It should give 0 coin for INR 1 when the set is Rs2");
+  test.assertEquals(vending.vendCoins(2, [2]), 1, "It should give 1 coin for INR 2 when the set is Rs2");
+  test.assertEquals(vending.vendCoins(5, [1, 2]), 3, "It should give 3 coin for INR 5 when the set is Rs1, Rs2");
+  test.assertEquals(vending.vendCoins(13, [1, 2, 5]), 4, "It should give 4 coin for INR 13 when the set is Rs1, Rs2, Rs5");
+  test.assertEquals(vending.vendCoins(13, [1, 2, 10]), 3, "It should give 3 coin for INR 13 when the set is Rs1, Rs2, Rs10");
+  test.assertEquals(vending.vendCoins(13, [1, 2, 10]), 3, "It should give 3 coin for INR 13 when the set is Rs1, Rs2, Rs10");
+  test.assertEquals(vending.vendCoins(18, [1, 2, 5, 10]), 4, "It should give 3 coin for INR 18 when the set is Rs1, Rs2, Rs5, Rs10");
 }
 vendingTest();
